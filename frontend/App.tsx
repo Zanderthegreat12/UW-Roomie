d771239bb1fd4d31dcab9abffe51c6bd8f709294
ccolbyssmith <46413250+ccolbyssmith@users.noreply.github.com>
2023-04-13 16:08:16 -0700

    messing with app.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
           <Text>I can see all the text {'>'}:)</Text>
+          <Text>Using type script</Text>
       <StatusBar style="auto" />
     </View>
   );
