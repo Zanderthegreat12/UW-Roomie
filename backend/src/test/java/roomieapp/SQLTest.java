ab9165286405420cd47169a6f8daa97d9eb07172
ccolbyssmith <46413250+ccolbyssmith@users.noreply.github.com>
2023-04-26 18:30:49 -0700

    Update SQLTest.java

--- backend/src/test/java/roomieapp/SQLTest.java
+++ backend/src/test/java/roomieapp/SQLTest.java
@@ -124,7 +124,7 @@ public class SQLTest
      * test setContactInfo creates new contactInfo for single user
      */
     @Test
-    public void testSetContactInfoSimple()
+    public void testCreateContactInfo()
     {
         Query querier = new Query();
         querier.clearTables();
@@ -136,10 +136,11 @@ public class SQLTest
                 "userTest#6969"
         );
         querier.setContactInfo(userContact);
+        querier.clearTables();
     }
 
     /**
-     *
+     * Test getting contact info for one user
      */
     @Test
     public void testGetContactInfoSimple()
@@ -147,7 +148,74 @@ public class SQLTest
         Query querier = new Query();
         querier.clearTables();
         querier.createUser("firstUser", "firstPassword");
+        ContactInfo userContact = new ContactInfo(
+                "firstUser",
+                "userTest@helpMe.com",
+                1028392074L,
+                "userTest#6969"
+        );
+        ContactInfo retrievedData = querier.getContactInfo("firstUser");
+        assertTrue(retrievedData.username.equals("firstUser"));
+        assertTrue(retrievedData.email.equals("userTest@helpMe.com"));
+        assertTrue(retrievedData.phoneNumber == 1028392074L);
+        assertTrue(retrievedData.discord.equals("userTest#6969"));
+        querier.clearTables();
+    }
 
+    /**
+     * Test setting contact info if user not in database
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetContactInfoNoUser()
+    {
+        Query querier = new Query();
+        querier.clearTables();
+        ContactInfo userContact = new ContactInfo(
+                "firstUser",
+                "userTest@helpMe.com",
+                1028392074L,
+                "userTest#6969"
+        );
+        querier.setContactInfo(userContact);
+        querier.clearTables();
+    }
+
+    /**
+     * test setContactInfo updates contactInfo for single user
+     */
+    @Test
+    public void testUpdateContactInfo()
+    {
+        Query querier = new Query();
+        querier.clearTables();
+        querier.createUser("user", "userPass");
+        ContactInfo userContact = new ContactInfo(
+                "user",
+                "userTest@helpMe.com",
+                1028392074L,
+                "userTest#6969"
+        );
+        querier.setContactInfo(userContact);
+        ContactInfo retrievedData = querier.getContactInfo("firstUser");
+        assertTrue(retrievedData.username.equals("firstUser"));
+        assertTrue(retrievedData.email.equals("userTest@helpMe.com"));
+        assertTrue(retrievedData.phoneNumber == 1028392074L);
+        assertTrue(retrievedData.discord.equals("userTest#6969"));
+
+        userContact = new ContactInfo(
+                "user",
+                "help@helpMe.com",
+                7392392074L,
+                "welp#9696"
+        );
+        querier.setContactInfo(userContact);
+        retrievedData = querier.getContactInfo("user");
+        assertTrue(retrievedData.username.equals("user"));
+        assertTrue(retrievedData.email.equals("help@helpMe.com"));
+        assertTrue(retrievedData.phoneNumber == 7392392074L);
+        assertTrue(retrievedData.discord.equals("welp#9696"));
+
+        querier.clearTables();
     }
 
     @Test
