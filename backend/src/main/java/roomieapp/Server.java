5f348a16725ef4f87d2ae66b5cd75c303cb9a997
ccolbyssmith <46413250+ccolbyssmith@users.noreply.github.com>
2023-05-01 12:24:21 -0700

    sparkjava set up

--- backend/src/main/java/roomieapp/Server.java
+++ backend/src/main/java/roomieapp/Server.java
@@ -1,5 +1,23 @@
 package roomieapp;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import com.google.gson.Gson;
+
+import static spark.Spark.*;
+
 class Server {
+
+    //http://localhost:4567
+    public static void main(String[] args) {
+        Spark.get("/hello", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "Hello World";
+            }
+        });
+    }
 
 }
