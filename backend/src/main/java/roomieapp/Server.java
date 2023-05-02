dd392b85517f46209a21991723ef7de7d67797c8
imcquade <imcquade@uw.edu>
2023-05-02 13:35:43 -0700

    Server updated

--- backend/src/main/java/roomieapp/Server.java
+++ backend/src/main/java/roomieapp/Server.java
@@ -6,13 +6,57 @@ import spark.Route;
 import spark.Spark;
 import com.google.gson.Gson;
 
+import java.util.List;
+
 import static spark.Spark.*;
 
 class Server {
-
+    final static Query q = new Query();
     //http://localhost:4567
     public static void main(String[] args) {
+        //Test to make sure its working
         Spark.get("/hello", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "Hello World";
+            }
+        });
+
+        //Gets the first k matches from the database
+        Spark.get("/get_match", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String username = request.queryParams("username");
+                //int numMatch = Integer.parseInt(request.queryParams("numMatch"));
+                //List<String> matches = q.getTopMatches(username, numMatch);
+                /*Gson g = new Gson();
+                String jsonResponse = g.toJson(matches);
+                return jsonResponse;*/
+                return "hello";
+            }
+        });
+
+        //Runs the algorithm and generates matches to put into the database.
+        Spark.get("/run_alg", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                //Takes every person who isn't the user from the database
+                //Runs them through the matching algorithm
+                //Puts them back into the database under matches
+                return "Hello World";
+            }
+        });
+
+        //Gets the contact info and returns it
+        Spark.get("/contact", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "Hello World";
+            }
+        });
+
+        //Checks to make sure the person's account is one within the database
+        Spark.get("/logIn", new Route() {
             @Override
             public Object handle(Request request, Response response) throws Exception {
                 return "Hello World";
