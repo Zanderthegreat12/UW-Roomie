2c8020f2432cb38dd499ffb57f56831c065fc400
Zanderthegreat12 <Zanderthegreat12@gmail.com>
2023-05-03 21:40:33 -0700

    Added Weights to Algorithm and added a method to MatchingAlgorithm

--- backend/src/main/java/roomieapp/MatchingAlgorithm.java
+++ backend/src/main/java/roomieapp/MatchingAlgorithm.java
@@ -1,5 +1,8 @@
 package roomieapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class MatchingAlgorithm {
 
     public MatchingAlgorithm(){}
@@ -21,22 +24,22 @@ class MatchingAlgorithm {
 
         // compare First Dorm
         if (user1.firstDorm.equals(user2.firstDorm)) {
-            comp+=5;
+            comp+=10;
         }
 
         // compare Second Dorm
         if (user1.secondDorm.equals(user2.secondDorm)) {
-            comp+=5;
+            comp+=7;
         }
 
         // compare Third Dorm
         if (user1.thirdDorm.equals(user2.thirdDorm)) {
-            comp+=5;
+            comp+=3;
         }
 
         // compare Room Type
         if (user1.roomType == user2.roomType){
-            comp+=5;
+            comp+=20;
         }
 
         // compare genderInclusiveness
@@ -46,20 +49,20 @@ class MatchingAlgorithm {
 
         // compare student Year
         if(user1.studentYear == user2.roommateYear && user1.roommateYear == user2.studentYear) {
-            comp+=5;
+            comp+=10;
         } else if (user1.roommateYear == -1 ) {
             if (user1.studentYear == user2.roommateYear || user2.roommateYear == -1){
-                comp+=5;
+                comp+=10;
             }
         } else if (user2.roommateYear == -1 ) {
             if (user2.studentYear == user1.roommateYear) {
-                comp+=5;
+                comp+=10;
             }
         }
 
         // compare drinking and Smoke preferences
         if (user1.drinkingPref == user2.drinkingPref){
-            comp+=5;
+            comp+=10;
         }
 
         // compare time you go to sleep
@@ -74,7 +77,7 @@ class MatchingAlgorithm {
 
         // compare the kind of sleeper you are
         if (user1.heavySleep == user2.heavySleep){
-            comp+=5;
+            comp+=6;
         }
 
         // compare the kind of vert that you are
@@ -94,31 +97,31 @@ class MatchingAlgorithm {
         if(user1.roommateFriends == 1 || user2.roommateFriends == 1){
             if(user1.roommateFriends == 1 && user2.roommateFriends == 1){
                 if(user2.studentFriends != 1 && user1.studentFriends !=1){
-                    comp+=5;
+                    comp+=7;
                 }
             } else if (user1.roommateFriends == 1){
                 if(user2.studentFriends != 1) {
-                    comp += 5;
+                    comp += 7;
                 }
             } else {
                 if(user1.studentFriends != 1){
-                    comp +=5;
+                    comp +=7;
                 }
             }
         } else {
-            comp+=5;
+            comp+=7;
         }
 
         // compare student neatness
         if(user1.studentNeat == user2.roommateNeat && user1.roommateNeat == user2.studentNeat) {
-            comp+=5;
+            comp+=7;
         } else if (user1.roommateNeat == -1 ) {
             if (user1.studentNeat == user2.roommateNeat || user2.roommateNeat == -1){
-                comp+=5;
+                comp+=7;
             }
         } else if (user2.roommateNeat == -1 ) {
             if (user2.studentNeat == user1.roommateNeat) {
-                comp+=5;
+                comp+=7;
             }
         }
 
@@ -129,4 +132,24 @@ class MatchingAlgorithm {
         }
     }
 
+    /**
+     * Calculate the compatibility for a single user with all other people using the app
+     * @param user1 The user whose compatability with everyone you want to calculate
+     * @param others A list of all the user surveys in the database
+     * @return A list of all the matches for the user and their compatability
+     */
+    public List<Match> ComputeCompatabilityForAll(Survey user1, List<Survey> others){
+
+        List<Match> matches = new ArrayList<>();
+
+        for(int i = 0; i < others.size(); i++){
+
+           if (user1.username != others.get(i).username){
+               Match pair = ComputeCompatability(user1, others.get(i));
+               matches.add(pair);
+           }
+
+       }
+       return matches;
+    }
 }
--- backend/src/test/java/roomieapp/MatchingAlgorithmTest.java
+++ backend/src/test/java/roomieapp/MatchingAlgorithmTest.java
@@ -1,83 +1,109 @@
 package roomieapp;
 
 import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 
 public class MatchingAlgorithmTest {
 
     @Test
-    public void testAllandNone(){
+    public void testAllandNone() {
 
+        Survey user1 = new Survey("a", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, 0, 0, 1, 0, 0, "Hello");
+        Survey user2 = new Survey("b", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, 0, 0, 1, 0, 0, "Hello");
+
+        MatchingAlgorithm m = new MatchingAlgorithm();
+
+        assertTrue(m.ComputeCompatability(user1, user2).equals(new Match("a", "b", 100, 0)));
+        assertTrue(m.ComputeCompatability(user2, user1).equals(new Match("a", "b", 100, 0)));
+
+        Survey user3 = new Survey("c", "B", "c", "D", 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "Sup");
+
+        m.ComputeCompatability(user1, user3);
+        assertTrue(m.ComputeCompatability(user1, user3).equals(new Match("a", "c", 0, 0)));
+        assertTrue(m.ComputeCompatability(user3, user1).equals(new Match("a", "c", 0, 0)));
+
+        assertTrue(m.ComputeCompatability(user1, user1) == null);
+
+
+    }
+
+    @Test
+    public void testIdontCare() {
+        Survey user1 = new Survey("a", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, -1, 0, 1, 0, 0, "Hello");
+        Survey user2 = new Survey("b", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 1, -1, 0, 1, 0, 0, "Hello");
+        Survey user3 = new Survey("c", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, 0, 0, 1, 0, 0, "Hello");
+        Survey user4 = new Survey("d", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 1, 0, 0, 1, 0, 0, "Hello");
+
+        MatchingAlgorithm m = new MatchingAlgorithm();
+
+        assertTrue(m.ComputeCompatability(user1, user2).equals(new Match("a", "b", 100, 0)));
+        assertTrue(m.ComputeCompatability(user2, user1).equals(new Match("a", "b", 100, 0)));
+
+        assertTrue(m.ComputeCompatability(user1, user3).equals(new Match("a", "c", 100, 0)));
+        assertTrue(m.ComputeCompatability(user3, user1).equals(new Match("a", "c", 100, 0)));
+
+        assertTrue(m.ComputeCompatability(user1, user4).equals(new Match("a", "d", 100, 0)));
+        assertTrue(m.ComputeCompatability(user4, user1).equals(new Match("a", "d", 100, 0)));
+
+        assertTrue(m.ComputeCompatability(user2, user4).equals(new Match("b", "d", 95, 0)));
+        assertTrue(m.ComputeCompatability(user4, user2).equals(new Match("b", "d", 95, 0)));
+
+        assertTrue(m.ComputeCompatability(user3, user4).equals(new Match("c", "d", 95, 0)));
+        assertTrue(m.ComputeCompatability(user4, user3).equals(new Match("c", "d", 95, 0)));
+    }
+
+    @Test
+    public void testFriends() {
+        Survey user1 = new Survey("a", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, 0, 0, 0, 0, 0, "Hello");
+        Survey user2 = new Survey("b", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, 0, 1, 0, 0, 0, "Hello");
+        Survey user3 = new Survey("c", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, 0, 0, 1, 0, 0, "Hello");
+        Survey user4 = new Survey("d", "a", "b", "c", 2, 1, 2, 2, 0, 8, 11, 0, 0, 0, 1, 1, 0, 0, "Hello");
+
+        MatchingAlgorithm m = new MatchingAlgorithm();
+
+        assertTrue(m.ComputeCompatability(user1, user2).equals(new Match("a", "b", 100, 0)));
+        assertTrue(m.ComputeCompatability(user2, user1).equals(new Match("a", "b", 100, 0)));
+
+        assertTrue(m.ComputeCompatability(user1, user3).equals(new Match("a", "c", 100, 0)));
+        assertTrue(m.ComputeCompatability(user3, user1).equals(new Match("a", "c", 100, 0)));
+
+        assertTrue(m.ComputeCompatability(user1, user4).equals(new Match("a", "d", 100, 0)));
+        assertTrue(m.ComputeCompatability(user4, user1).equals(new Match("a", "d", 100, 0)));
+
+        assertTrue(m.ComputeCompatability(user3, user2).equals(new Match("b", "c", 93, 0)));
+        assertTrue(m.ComputeCompatability(user2, user3).equals(new Match("b", "c", 93, 0)));
+
+        assertTrue(m.ComputeCompatability(user2, user4).equals(new Match("b", "d", 93, 0)));
+        assertTrue(m.ComputeCompatability(user4, user2).equals(new Match("b", "d", 93, 0)));
+
+        assertTrue(m.ComputeCompatability(user3, user4).equals(new Match("c", "d", 93, 0)));
+        assertTrue(m.ComputeCompatability(user4, user3).equals(new Match("c", "d", 93, 0)));
+
+    }
+
+    @Test
+    public void testCompAll(){
         Survey user1 = new Survey("a","a","b","c",2,1,2,2,0,8,11,0,0,0,0,1,0,0, "Hello");
         Survey user2 = new Survey("b","a","b","c",2,1,2,2,0,8,11,0,0,0,0,1,0,0, "Hello");
+        Survey user3 = new Survey("c", "B", "c", "D", 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "Sup");
 
-        MatchingAlgorithm m = new MatchingAlgorithm();
+        List<Survey> surveys = new ArrayList<>();
+        surveys.add(user1);
+        surveys.add(user2);
+        surveys.add(user3);
 
-        assertTrue(m.ComputeCompatability(user1,user2).equals(new Match("a","b",65,0)));
-        assertTrue(m.ComputeCompatability(user2,user1).equals(new Match("a","b",65,0)));
+        MatchingAlgorithm M = new MatchingAlgorithm();
 
-        Survey user3 = new Survey("c","B","c","D", 1,0,1,1,1,1,1,1,1,1,1,1,1,1,"Sup");
+        List<Match> matches =  M.ComputeCompatabilityForAll(user1, surveys);
 
-        m.ComputeCompatability(user1,user3);
-        assertTrue(m.ComputeCompatability(user1,user3).equals(new Match("a","c",0,0)));
-        assertTrue(m.ComputeCompatability(user3,user1).equals(new Match("a","c",0,0)));
+        List<Match> check = new ArrayList<>();
+        check.add(new Match("a","b",100,0));
+        check.add(new Match("a","c",0,0));
 
-        assertTrue(m.ComputeCompatability(user1,user1) == null);
-
-
-    }
-
-    @Test
-    public void testIdontCare(){
-        Survey user1 = new Survey("a","a","b","c",2,1,2,2,0,8,11,0,0,-1,0,1,0,0, "Hello");
-        Survey user2 = new Survey("b","a","b","c",2,1,2,2,0,8,11,0,1,-1,0,1,0,0, "Hello");
-        Survey user3 = new Survey("c","a","b","c",2,1,2,2,0,8,11,0,0,0,0,1,0,0, "Hello");
-        Survey user4 = new Survey("d","a","b","c",2,1,2,2,0,8,11,0,1,0,0,1,0,0, "Hello");
-
-        MatchingAlgorithm m = new MatchingAlgorithm();
-
-        assertTrue(m.ComputeCompatability(user1,user2).equals(new Match("a","b",65,0)));
-        assertTrue(m.ComputeCompatability(user2,user1).equals(new Match("a","b",65,0)));
-
-        assertTrue(m.ComputeCompatability(user1,user3).equals(new Match("a","c",65,0)));
-        assertTrue(m.ComputeCompatability(user3,user1).equals(new Match("a","c",65,0)));
-
-        assertTrue(m.ComputeCompatability(user1,user4).equals(new Match("a","d",65,0)));
-        assertTrue(m.ComputeCompatability(user4,user1).equals(new Match("a","d",65,0)));
-
-        assertTrue(m.ComputeCompatability(user2,user4).equals(new Match("b","d",60,0)));
-        assertTrue(m.ComputeCompatability(user4,user2).equals(new Match("b","d",60,0)));
-
-        assertTrue(m.ComputeCompatability(user3,user4).equals(new Match("c","d",60,0)));
-        assertTrue(m.ComputeCompatability(user4,user3).equals(new Match("c","d",60,0)));
-    }
-
-    @Test
-    public void testFriends(){
-        Survey user1 = new Survey("a","a","b","c",2,1,2,2,0,8,11,0,0,0,0,0,0,0, "Hello");
-        Survey user2 = new Survey("b","a","b","c",2,1,2,2,0,8,11,0,0,0,1,0,0,0, "Hello");
-        Survey user3 = new Survey("c","a","b","c",2,1,2,2,0,8,11,0,0,0,0,1,0,0, "Hello");
-        Survey user4 = new Survey("d","a","b","c",2,1,2,2,0,8,11,0,0,0,1,1,0,0, "Hello");
-
-        MatchingAlgorithm m = new MatchingAlgorithm();
-
-        assertTrue(m.ComputeCompatability(user1,user2).equals(new Match("a","b",65,0)));
-        assertTrue(m.ComputeCompatability(user2,user1).equals(new Match("a","b",65,0)));
-
-        assertTrue(m.ComputeCompatability(user1,user3).equals(new Match("a","c",65,0)));
-        assertTrue(m.ComputeCompatability(user3,user1).equals(new Match("a","c",65,0)));
-
-        assertTrue(m.ComputeCompatability(user1,user4).equals(new Match("a","d",65,0)));
-        assertTrue(m.ComputeCompatability(user4,user1).equals(new Match("a","d",65,0)));
-
-        assertTrue(m.ComputeCompatability(user3,user2).equals(new Match("b","c",60,0)));
-        assertTrue(m.ComputeCompatability(user2,user3).equals(new Match("b","c",60,0)));
-
-        assertTrue(m.ComputeCompatability(user2,user4).equals(new Match("b","d",60,0)));
-        assertTrue(m.ComputeCompatability(user4,user2).equals(new Match("b","d",60,0)));
-
-        assertTrue(m.ComputeCompatability(user3,user4).equals(new Match("c","d",60,0)));
-        assertTrue(m.ComputeCompatability(user4,user3).equals(new Match("c","d",60,0)));
-
+        assertTrue(matches.equals(check));
     }
 }
