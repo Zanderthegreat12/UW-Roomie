5f65b4561075878ba8ff3602623b2130e4e3c172
imcquade <imcquade@uw.edu>
2023-05-07 17:21:03 -0700

    Parameters can now be passed! Login passes username on to other screens.

--- frontend/components/LoginScreen.tsx
+++ frontend/components/LoginScreen.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {useNavigation} from "@react-navigation/native";
+import React, {useState} from 'react';
 
-export default function LoginScreen() {
+export default function LoginScreen({route}) {
     const navigation = useNavigation();
+    const [username, setUser] = useState(route.params.user);
+
     return (
         <View style={styles.container}>
-            <Text>Signed In</Text>
+            <Text>Welcome {username}</Text>
             <Button
                 title="Matching Menu"
                 onPress={() => navigation.navigate('Matching Menu')}
