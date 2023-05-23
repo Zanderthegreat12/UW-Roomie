f6842dc5aa67987efc85d24a22ef37714a834c1a
ccolbyssmith <46413250+ccolbyssmith@users.noreply.github.com>
2023-05-23 14:54:59 -0700

    database transferred

--- backend/src/main/java/roomieapp/DBConnUtils.java
+++ backend/src/main/java/roomieapp/DBConnUtils.java
@@ -35,18 +35,15 @@ public class DBConnUtils {
   }
 
   private static Connection getConn(Properties configProps) throws SQLException, IOException {
-    String hostname = configProps.getProperty("roomieapp.server_url");
-    String port = configProps.getProperty("roomieapp.port");
+    String serverURL = configProps.getProperty("roomieapp.server_url");
     String dbName = configProps.getProperty("roomieapp.database_name");
     String adminName = configProps.getProperty("roomieapp.username");
     String password = configProps.getProperty("roomieapp.password");
 
-    String connectionUrl = "jdbc:mysql://" + hostname + ":" + port +
-            "/" + dbName + "?user=" + adminName + "&password=" + password;
+    String connectionUrl =
+            String.format("jdbc:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s",
+                    serverURL, dbName, adminName, password);
     Connection conn = DriverManager.getConnection(connectionUrl);
-
-    // By default, automatically commit after each statement
-    conn.setAutoCommit(false);
 
     // By default, set the transaction isolation level to serializable
     conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
