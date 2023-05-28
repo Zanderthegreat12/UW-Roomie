bf63528b1de26f58a9fa8b07456e61778c5db31d
leahskye <leahskye@cs.washington.edu>
2023-05-28 16:06:28 -0700

    Added the rest of the LoginScreen button logos

--- frontend/components/LoginScreen.tsx
+++ frontend/components/LoginScreen.tsx
@@ -43,14 +43,22 @@ export default function LoginScreen({route}) {
             </View>
             <View style={{flexDirection:'row', height:'20%'}}>
                 <View style={[styles.button, {flex:1,},]}>
-                    <Button
-                        title="View Outgoing Matches"
-                        color="#7c2bee"
-                        onPress={() => navigation.navigate('Outgoing Matches Screen', {user: '' + username,})}
-                    />
+                    <View>
+                        <Image style = {styles.logo}
+                            source = {require('../assets/package.png')}>
+                        </Image>
+                        <Button
+                            title="View Outgoing Matches"
+                            color="#7c2bee"
+                            onPress={() => navigation.navigate('Outgoing Matches Screen', {user: '' + username,})}
+                        />
+                    </View>
                 </View>
                 <View style={[styles.button, {flex:1,},]}>
                     <View>
+                        <Image style = {styles.logo}
+                            source = {require('../assets/send.png')}>
+                        </Image>
                         <Button
                             title="Accept/Reject Oncoming Matches"
                             color="#7c2bee"
@@ -61,18 +69,28 @@ export default function LoginScreen({route}) {
             </View>
             <View style={{flexDirection:'row', height:'20%'}}>
                 <View style={[styles.button, {flex:1,},]}>
-                    <Button
-                        title="Your profile"
-                        color="#7c2bee"
-                        onPress={() => navigation.navigate('Profile', {user: '' + username,})}
-                    />
+                    <View>
+                        <Image style = {styles.logo}
+                            source = {require('../assets/user.png')}>
+                        </Image>
+                        <Button
+                            title="Your profile"
+                            color="#7c2bee"
+                            onPress={() => navigation.navigate('Profile', {user: '' + username,})}
+                        />
+                    </View>
                 </View>
                 <View style={[styles.button, {flex:1,},]}>
-                    <Button
-                        title="Log Out"
-                        color="#7c2bee"
-                        onPress={() => navigation.replace('Home')}//navigation.navigate('Home', {user: ''})}
-                    />
+                    <View>
+                        <Image style = {styles.logo}
+                            source = {require('../assets/log-out.png')}>
+                        </Image>
+                        <Button
+                            title="Log Out"
+                            color="#7c2bee"
+                            onPress={() => navigation.replace('Home')}//navigation.navigate('Home', {user: ''})}
+                        />
+                    </View>
                 </View>
             </View>
         </View>
@@ -102,9 +120,9 @@ const styles = StyleSheet.create({
     },
 
     button: {
-        padding: 10,
         justifyContent: 'center',
         flexGrow: 1,
+        padding: 15,
     },
 
     logo: {
